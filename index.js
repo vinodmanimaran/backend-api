1bed4a5677f4f9bae15b4377d4ce776f602d4cac
Vinod Kumar <vk5241415@gmail.com>
2024-02-19 15:03:59 +0530

    eeror- 1

--- index.js
+++ index.js
@@ -173,9 +173,19 @@ async function extractLocationAndDevice(req) {
 
         return { geoLocation, deviceInfo };
     } catch (error) {
-        console.error('Error extracting location and device:', error);
-        throw error; // Rethrow the error to be caught by the calling function
-    }
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log('Error response data:', error.response.data);
+            console.log('Error response status:', error.response.status);
+            console.log('Error response headers:', error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log('Error request:', error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error message:', error.message);
+        }    }
 }
 
 
